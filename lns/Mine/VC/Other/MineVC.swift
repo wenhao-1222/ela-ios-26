4bd88dfa83d9ea095fded25e8951524509f81e05
文浩 <510126950@qq.com>
2026-04-20 14:04:11 +0800

    删除旧的关于AI教练球动画的文件

--- lns/Mine/VC/Other/MineVC.swift
+++ lns/Mine/VC/Other/MineVC.swift
@@ -51,10 +51,7 @@ class MineVC : WHBaseViewVC {
             self.navigationController?.pushViewController(vc, animated: true)
         }
         vm.statVm.tapBlock = {()in
-//            let vc = NaturalStatVC()
-//            let vc = ColorWheelVC()
-            let vc = AICoachBreathingOrbDemoViewController()
-//            let vc = GuidanceProVC()
+            let vc = NaturalStatVC()
             self.navigationController?.pushViewController(vc, animated: true)
         }
         vm.mealVm.tapBlock = {()in
