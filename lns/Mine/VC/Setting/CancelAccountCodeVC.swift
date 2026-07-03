674f9e3be0a2f34379fd4550e9a9e070804d9488
文浩 <510126950@qq.com>
2026-07-03 14:55:45 +0800

    256

--- lns/Mine/VC/Setting/CancelAccountCodeVC.swift
+++ lns/Mine/VC/Setting/CancelAccountCodeVC.swift
@@ -234,6 +234,7 @@ extension CancelAccountCodeVC{
             UserDefaults.standard.setValue("", forKey: userId)
             UserDefaults.set(value: "", forKey: .myFoodsList)
             UserDefaults.set(value: "", forKey: .hidsoryFoodsAdd)
+            ElaProIAPManager.shared.resetAnonymousIdentityAfterAccountDeletion()
             UserInfoModel.shared.clearMsg()
         }
     }
